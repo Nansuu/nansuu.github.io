3a23a1d5d310f57c09947b8a1336df39b1dc2bc3
Nansuu <118701671+Nansuu@users.noreply.github.com>
2024-05-20 17:36:12 +0900

    Deploy react app 27

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -26,7 +26,7 @@ function App() {
         <div className="App">
         <h1>Welcome to My React App</h1>
         <p>This is a simple React application.</p>
-        <button onClick={handleClick}>Click 1!</button>
+        <button onClick={handleClick}>Click 2!</button>
     </div>
       </header>
     </div>
